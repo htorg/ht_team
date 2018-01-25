11f5e989b9f5dfd16637b91e1d3ce715fd56bce8
Lenovo-PC\htshen <shenhongteng@163.com>
2018-01-25 10:38:22 +0800

    增加注册

--- frontend/themes/t00001/views/layouts/main.php
+++ frontend/themes/t00001/views/layouts/main.php
@@ -168,8 +168,9 @@ use common\helpers\ThemeHelper;
 <!--    </div>-->
 <!--</div>-->
 
+<!--frame_signIn-->
 <div class="frame_signIn  frame_bx " style="display:none;">
-    <i class="iconfont delete_df btn_df">&#xe625;</i>
+    <i class="iconfont delete_df btn_df noneSelect">&#xe625;</i>
     <img class="logo_df" src="../themes/t00001/dist/img/frame_logo.png">
     <form class="frame_form">
         <div class="input_bx">
@@ -178,9 +179,8 @@ use common\helpers\ThemeHelper;
         </div>
         <div class="input_bx">
             <i class="ico_password"></i>
-            <input class="writeIn" type="text" placeholder="登录密码">
+            <input class="writeIn" type="password" placeholder="登录密码">
         </div>
-
         <p class="btn_row">
             <a class="btn_df" href="#">立即注册</a>|<a class="btn_df" href="#">忘记密码</a>
         </p>
@@ -189,6 +189,57 @@ use common\helpers\ThemeHelper;
     </form>
 </div>
 
+<!--frame_register_message-->
+<div class="frame_register_message  frame_bx " >
+    <i class="iconfont delete_df btn_df noneSelect">&#xe625;</i>
+    <img class="logo_df" src="../themes/t00001/dist/img/frame_logo.png">
+    <form class="frame_form">
+        <div class="input_bx">
+            <i class="ico_number"></i>
+            <input class="writeIn" type="text" placeholder="手机号">
+        </div>
+
+        <div class="verify_code">
+            <div class="input_bx">
+                <i class="ico_code"></i>
+                <input class="writeIn" type="text" placeholder="验证码">
+            </div>
+            <button class="getVerify_btn btn_df" type="button" >获取短信验证码</button>
+        </div>
+
+        <div class="check_bx">
+            <input id="sigIn_protocol" type="checkbox" >
+            <label class="sigIn_protocol_label noneSelect " for="sigIn_protocol"></label>
+            <span class="words_df noneSelect">我已阅读并同意<a class="btn_df">《新物种用户注册协议》<a></a></span>
+        </div>
+
+        <a class="btn_df frame_btn noneSelect">注册</a>
+    </form>
+</div>
+
+
+<!--frame_register_account-->
+<div class="frame_register_account  frame_bx " style="display:none;">
+    <i class="iconfont delete_df btn_df noneSelect">&#xe625;</i>
+    <img class="logo_df" src="../themes/t00001/dist/img/frame_logo.png">
+    <form class="frame_form">
+        <div class="input_bx">
+            <i class="ico_account"></i>
+            <input class="writeIn" type="text" placeholder="昵称">
+        </div>
+
+        <div class="input_bx">
+            <i class="ico_password"></i>
+            <input class="writeIn" type="password" placeholder="设置密码">
+        </div>
+
+        <div class="input_bx again_input">
+            <i class="ico_password"></i>
+            <input class="writeIn" type="password" placeholder="确认密码">
+        </div>
+        <a class="btn_df frame_btn noneSelect">完成</a>
+    </form>
+</div>
 <?php $this->endBody() ?>
 </body>
 </html>
